77cc9746143866f0cc177f19b321599312142de1
Shimil <shimil@smartdatainc.net>
2017-07-18 15:01:47 +0530

    Tab bar Completed

--- Test.js
+++ Test.js
@@ -1,21 +1,65 @@
-import React, { PureComponent } from 'react';
-import { View, StyleSheet,ScrollView,Text   } from 'react-native';
-import ScrollableTabView, {DefaultTabBar, } from 'react-native-scrollable-tab-view';
+import React, { Component } from 'react';
+import {
+  StyleSheet,   // CSS-like styles
+  Text,         // Renders text
+  View          // Container component
+} from 'react-native';
+import ScrollTabBar from 'app/components/ScrollTabBar';
+const data=['tab1','tab2','tab3','tab5','tab6','tab7','tab8']
+export default class App extends Component {
 
+renderTabs=()=>{
+  return(
 
-const Data=['map','video','pol','klio','mam']
+  data.map((key,index)=>(
 
-export default React.createClass({
+         <View title={key} style={styles.content} key={index}>
+            <Text style={styles.header}>
+              {key}
+            </Text>
+          </View>
+  ))
+
+)
+}
   render() {
-    return(
-      <ScrollableTabView
-      style={{marginTop: 20, }}
-      renderTabBar={() => <DefaultTabBar />}
-    >
-      {Data.map((index,key)=>(<Text key={index} tabLabel={index}>{key}</Text>))}
-    </ScrollableTabView>
-    )
+    return (
+      <View style={styles.container}>
 
-
+      <ScrollTabBar tabstyle={{backgroundColor:'#939ceb'}}>
+        {this.renderTabs()}
+       </ScrollTabBar>
+      </View>
+    );
   }
+}
+
+const styles = StyleSheet.create({
+  // App container
+  container: {
+    flex: 1,                            // Take up all screen
+    backgroundColor:'#939ceb'     // Background color
+  },
+  // Tab content container
+  content: {
+    flex: 1,                            // Take up all available space
+    justifyContent: 'center',           // Center vertically
+    alignItems: 'center',               // Center horizontally
+    backgroundColor: '#C2185B',         // Darker background for content area
+  },
+  // Content header
+  header: {
+    margin: 10,                         // Add margin
+    color: '#FFFFFF',                   // White color
+    fontFamily: 'Avenir',               // Change font family
+    fontSize: 26,                       // Bigger font size
+  },
+  // Content text
+  text: {
+    marginHorizontal: 20,               // Add horizontal margin
+    color: 'rgba(255, 255, 255, 0.75)', // Semi-transparent text
+    textAlign: 'center',                // Center
+    fontFamily: 'Avenir',
+    fontSize: 18,
+  },
 });
