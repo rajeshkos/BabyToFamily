8519b2f9078bec53fa3d890ec6117ece1ac99c54
Shimil <shimil@smartdatainc.net>
2017-07-19 17:17:21 +0530

    Drawer Page Created

---
+++ src/containers/Drawer/DrawerPages/InviteEarn/InviteEarn.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import {View,Text} from 'react-native';
+
+
+export default class  InviteEarn extends Component{
+  render(){
+    return(
+      <View style={{flex:1}}>
+      <Text style={{color:'red'}}>FamilyMember</Text>
+      </View>
+    )
+  }
+}
